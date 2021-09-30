53e963c4c627a2a04574ec107a9a3da683c9f1a2
Aldrin Natividad <aldmnatividad@gmail.com>
2021-09-30 16:34:24 -0400

    Create grid for layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, createTheme, CssBaseline, TextField } from '@material-ui/core';
+import { Box, Button, Container, createTheme, CssBaseline, Grid, makeStyles, TextField } from '@material-ui/core';
 import { ThemeProvider } from '@material-ui/styles';
 
 import firebase from 'firebase/compat/app';
@@ -9,6 +9,7 @@ import 'firebase/compat/auth';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 import SignIn from './components/SignIn';
+import SignInBox from './components/SignInBox';
 
 const darkTheme = createTheme({
   palette: {
@@ -25,17 +26,37 @@ firebase.initializeApp({
   appId: "1:438322523913:web:8258be948ab81d86616dd8"
 });
 
+const useStyles = makeStyles(() => ({
+  mainLayoutGrid: {
+    minHeight: "100vh",
+    minWidth: "100vw"
+  },
+  verticalMarginGrid: {
+    minHeight: "20vh"
+  },
+  contentGrid: {
+    minHeight: "60vh"
+  }
+}));
+
 const auth = firebase.auth();
 const firestore = firebase.firestore();
 
 function App() {
+  const classes = useStyles();
   const [user] = useAuthState(auth);
 
   return (
     <ThemeProvider theme={darkTheme}>
       <CssBaseline />
       <div class="page-bg"/>
-      {user ? <TextField/> : <SignIn auth={auth} firebase={firebase}/>}
+      <Grid container className={classes.mainLayoutGrid}>
+        <Grid xs={12} className={classes.verticalMarginGrid}/>
+        <Grid xs={2} className={classes.contentGrid}/>
+        <Grid xs={8} className={classes.contentGrid}><SignInBox user={user} auth={auth} firebase={firebase}/></Grid>
+        <Grid xs={2} className={classes.contentGrid}/>
+        <Grid xs={12} className={classes.verticalMarginGrid}/>
+      </Grid>
     </ThemeProvider>
   );
 }
