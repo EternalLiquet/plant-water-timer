9410422bf7f77158cb3d546937fb6ccdf05c9d81
Aldrin Dominique Natividad <aldmnatividad@gmail.com>
2021-10-02 15:19:31 -0400

    Implement auth changes and create query for firestore

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Button, Container, createTheme, CssBaseline, Grid, makeStyles, TextField } from '@material-ui/core';
+import { AppBar, Box, Button, Container, createTheme, CssBaseline, Grid, makeStyles, TextField, Toolbar, Typography } from '@material-ui/core';
 import { ThemeProvider } from '@material-ui/styles';
 
 import firebase from 'firebase/compat/app';
@@ -10,6 +10,8 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 import SignIn from './components/SignIn';
 import SignInBox from './components/SignInBox';
+import SignOut from './components/SignOut';
+import AccountSignedIn from './components/AccountSignedIn';
 
 const darkTheme = createTheme({
   palette: {
@@ -28,14 +30,16 @@ firebase.initializeApp({
 
 const useStyles = makeStyles(() => ({
   mainLayoutGrid: {
-    minHeight: "100vh",
-    minWidth: "100vw"
+    minHeight: "95vh",
+    width: "100vw",
+    maxWidth: "100%"
   },
   verticalMarginGrid: {
-    minHeight: "20vh"
+    minHeight: "10%",
+    maxHeight: "10%"
   },
   contentGrid: {
-    minHeight: "60vh"
+    minHeight: "80%"
   }
 }));
 
@@ -45,15 +49,34 @@ const firestore = firebase.firestore();
 function App() {
   const classes = useStyles();
   const [user] = useAuthState(auth);
+  const plantsRef = firestore.collection("plants");
+  const query = plantsRef.where("uid", "==", user ? auth.currentUser.uid : '')
+  const [plants] = useCollectionData(query, { idField: 'id' });
 
   return (
     <ThemeProvider theme={darkTheme}>
       <CssBaseline />
-      <div class="page-bg"/>
+      <div class="page-bg" />
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static" style={{ background: "#127A42"}}>
+          <Toolbar>
+            <Typography>Plant Water Reminder</Typography>
+            <div style={{ flexGrow: 1}}/>
+            {user ? <AccountSignedIn auth={auth} firebase={firebase}/> : <SignIn auth={auth} firebase={firebase}/>}
+          </Toolbar>
+        </AppBar>
+      </Box>
       <Grid container className={classes.mainLayoutGrid}>
         <Grid xs={12} className={classes.verticalMarginGrid}/>
         <Grid xs={2} className={classes.contentGrid}/>
-        <Grid xs={8} className={classes.contentGrid}><SignInBox user={user} auth={auth} firebase={firebase}/></Grid>
+        <Grid xs={8} className={classes.contentGrid}>
+          {plants && plants.map(plant =>
+            <Container key={plant.id}>
+              <Typography>Plant Name: {plant.plantName}</Typography>
+              <Typography>Last Watered Date: {plant.lastWatering.toDate().toString()}</Typography>
+              <Typography>Next Watering Date: {plant.nextWatering.toDate().toString()}</Typography>
+            </Container>)}
+        </Grid>
         <Grid xs={2} className={classes.contentGrid}/>
         <Grid xs={12} className={classes.verticalMarginGrid}/>
       </Grid>
