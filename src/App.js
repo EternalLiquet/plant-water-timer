6310047f602b2dd8281fbbf9b2e11d955e6c0fb6
Aldrin Natividad <aldmnatividad@gmail.com>
2021-10-06 15:52:21 -0400

    Add modal

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import { AppBar, Box, Button, createTheme, CssBaseline, Grid, makeStyles, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Toolbar, Typography } from '@material-ui/core';
+import React, { useState } from 'react';
+import { AppBar, Box, Button, createTheme, CssBaseline, FormControl, Grid, makeStyles, Modal, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TextField, Toolbar, Typography } from '@material-ui/core';
 import { ThemeProvider } from '@material-ui/styles';
+import { useForm } from 'react-hook-form';
 
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/firestore';
@@ -38,6 +39,17 @@ const useStyles = makeStyles(() => ({
   },
   contentGrid: {
     minHeight: "80%"
+  }, 
+  addPlantModal: {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4
   }
 }));
 
@@ -50,6 +62,8 @@ function App() {
   const plantsRef = firestore.collection("plants");
   const query = plantsRef.where("uid", "==", user ? auth.currentUser.uid : '')
   const [plants] = useCollectionData(query, { idField: 'id' });
+  const [openAddPlant, setOpenAddPlant] = useState(false);
+  const { register, handleSubmit } = useForm();
   var content;
 
   const handleWater = (plant) => {
@@ -57,6 +71,22 @@ function App() {
     var newDateForWatering = new Date(new Date().getTime() + (plant.wateringGap * (1000 * 3600 * 24)));
     plantsRef.doc(plant.id).update({ lastWatering: new Date(), nextWatering: newDateForWatering });
   }
+
+  const onSubmit = (form) => {
+    alert(JSON.stringify(form));
+  }
+
+  const modalStyle = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
 
   if(user) {
     content = <TableContainer component={Paper}>
@@ -89,7 +119,33 @@ function App() {
           <TableCell />
           <TableCell />
           <TableCell>
-            <Button variant="contained">Add New Plant</Button>
+            <Button variant="contained" onClick={() => setOpenAddPlant(!openAddPlant)}>Add New Plant</Button>
+            <Modal
+              open={openAddPlant}
+              onClose={() => setOpenAddPlant(false)}
+            >
+              <Box sx={modalStyle}>
+              <form onSubmit={handleSubmit(onSubmit)}>
+                <FormControl>
+                  <TextField
+                    name="plantName"
+                    label="Plant Name"
+                    variant="outlined"
+                    inputRef={register()}
+                  />
+                  <TextField
+                    name="daysToWater"
+                    label="Days Between Watering"
+                    variant="outlined"
+                    type="number"
+                    inputRef={register()}
+                  />
+                  <Button variant="contained">Add Plant</Button>
+                </FormControl>
+                
+              </form>
+              </Box>
+            </Modal>
           </TableCell>
         </TableRow>
       </TableBody>
